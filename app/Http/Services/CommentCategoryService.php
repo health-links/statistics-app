3cfc73ecd93d1622698bf87e09c96cdd5663063b
ahmed helmy <helmyahmed248@gmail.com>
2022-06-08 22:28:03 +0200

    debug filter

--- app/Http/Services/CommentCategoryService.php
+++ app/Http/Services/CommentCategoryService.php
@@ -24,15 +24,6 @@ class CommentCategoryService
 
     public function getCategoriesData()
     {
-        // $categories = DB::table('comments_categories')
-        //     ->join('comment_category', 'comment_category.category_id', '=', 'comments_categories.c_id')
-        //     ->join('comments_api', 'comments_api.sn_id', '=', 'comment_category.comment_id')
-        //     ->select('comment_category.category_id', 'comments_categories.c_name as c_name', 'comments_api.sn_id')
-        //     ->selectRaw("count(CASE WHEN comments_api.sn_id = comment_category.comment_id and comments_api.r_rate = 'positive' THEN 1 END) AS positive_count")
-        //     ->selectRaw("count(CASE WHEN comments_api.sn_id = comment_category.comment_id and comments_api.r_rate = 'negative' THEN 1 END) AS negative_count")
-        //     ->selectRaw("count(CASE WHEN comments_api.sn_id = comment_category.comment_id and comments_api.r_rate = 'neutral' THEN 1 END) AS neutral_count")
-        //     ->groupBy('comments_categories.c_id', 'comments_categories.c_name')
-        //     ->get();
 
         $categories = $this->getCommentCategory()
             ->when(request()->filter && request()->filter['from'], function ($q) {
@@ -53,22 +44,6 @@ class CommentCategoryService
     }
     public function getHeatMapData()
     {
-        // $categories = DB::table('comments_categories')
-        //     ->join('comment_category', 'comment_category.category_id', '=', 'comments_categories.c_id')
-        //     ->when(request()->filter && request()->filter['client_id'], function ($q) {
-        //         $q->join('comments_api as comments_Client', 'comments_Client.sn_id', '=', 'comment_category.comment_id')
-        //             ->where('comments_Client.sn_client', request()->filter['client_id']);
-        //     })
-        //     ->when(request()->filter && request()->filter['service_id'], function ($q) {
-        //         $q->join('comments_api as comments_service', 'comments_service.sn_id', '=', 'comment_category.comment_id')
-        //             ->where('comments_service.sn_service', request()->filter['service_id']);
-        //     })
-        //     ->join('comment_topic', 'comment_topic.comment_id', '=', 'comment_category.comment_id')
-        //     ->join('comments_topics', 'comments_topics.t_id', '=', 'comment_topic.topic_id')
-        //     ->select('comments_categories.c_name as category_name', 'comment_category.comment_id', 'comments_topics.t_name as topic_name', 'comment_topic.topic_id', DB::raw('count(comment_topic.topic_id) as count'))
-        //     ->groupBy('comments_categories.c_name', 'comments_topics.t_name', 'comment_topic.topic_id')
-        //     ->get();
-
         $categories = $this->getCommentCategory()
             ->join('comment_topic', 'comment_topic.comment_id', '=', 'comment_category.comment_id')
             ->join('comments_topics', 'comments_topics.t_id', '=', 'comment_topic.topic_id')
